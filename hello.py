7b26a02425f592e02bf2615af9519aa480fc1d94
本多優介 <hondayuusuke@hondayuusukes-MacBook-Air.local>
2026-02-21 17:17:38 +0900

    calculate next year age

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 name = input("What is your name? ")
-age = input("How old are you? ")
+age = int(input("How old are you? "))
 
 print(f"Hello, {name}!")
 print(f"You are {age} years old.")
+print(f"Next year, you will be {age + 1}.")
